98360fef780b498177d5dd1f84904b8c6b363782
Norton Santos <norton.santos@NAVASLINS.LOCAL>
2023-10-25 15:49:58 -0300

    feat: fetching products

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
+import pandas as pd
 import requests
-import json
 from dotenv import load_dotenv
 import os
 
-# Carrega as variáveis de ambiente do arquivo .env
 load_dotenv()
 
-url = "https://api.openai.com/v1/chat/completions"
-headers = {
-    "Content-Type": "application/json",
-    "Authorization": "Bearer " + os.getenv("OPENAI_API_KEY")
-}
-data = {
-    "model": "gpt-3.5-turbo",
-    "messages": [{"role": "user", "content": "Say this is a test!"}],
-    "temperature": 0.7
-}
 
-response = requests.post(url, headers=headers, data=json.dumps(data))
+def gerar_seo_produtos(lista_produtos):
+    url = "https://api.openai.com/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer " + os.getenv("OPENAI_API_KEY")
+    }
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [{"role": "user", "content": f"Gere a descrição SEO da seguinte lista de Produtos: {', '.join(lista_produtos)}"}],
+        "temperature": 0.7
+    }
 
-print(response.json())
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    return response.json()
+
+planilha = pd.read_excel('sheet.xlsx')
+
+
+lista_produtos = []
+for index, row in planilha.iterrows():
+    cod_prod = row['Codprod']
+    descricao = row['Descrição']
+    marca = row['Marca']
+    lista_produtos.append(f"Produto: {cod_prod}, Descrição: {descricao}, Marca: {marca}")
+
+
+seo_produtos = gerar_seo_produtos(lista_produtos)
+
+planilha['SEO'] = seo_produtos
+
+planilha.to_excel('new_sheet.xlsx', index=False)
