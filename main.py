b6ef6f6e9b2a1eea564ced5dc1a3913d86976953
Norton Santos <nortonsantos79@gmail.com>
2023-10-25 16:26:20 -0300

    feat: saving in new sheet

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pandas as pd
 import requests
 from dotenv import load_dotenv
 import os
+import json
+
 
 load_dotenv()
 
@@ -14,26 +16,35 @@ def gerar_seo_produtos(lista_produtos):
     }
     data = {
         "model": "gpt-3.5-turbo",
-        "messages": [{"role": "user", "content": f"Gere a descrição SEO da seguinte lista de Produtos: {', '.join(lista_produtos)}"}],
+        "messages": [{"role": "user", "content": f"Produtos: {', '.join(lista_produtos)}"}],
         "temperature": 0.7
     }
 
     response = requests.post(url, headers=headers, data=json.dumps(data))
+    print (response)
     return response.json()
 
-planilha = pd.read_excel('sheet.xlsx')
+
+caminho_pasta = 'sheets/'
+
+
+caminho_planilha = os.path.join(caminho_pasta, 'sheet.xlsx')
+planilha = pd.read_excel(caminho_planilha)
 
 
 lista_produtos = []
 for index, row in planilha.iterrows():
-    cod_prod = row['Codprod']
-    descricao = row['Descrição']
-    marca = row['Marca']
+    cod_prod = row['codprod']
+    descricao = row['descrprod']
+    marca = row['marca']
     lista_produtos.append(f"Produto: {cod_prod}, Descrição: {descricao}, Marca: {marca}")
 
 
-seo_produtos = gerar_seo_produtos(lista_produtos)
+try:
+    seo_produtos = gerar_seo_produtos(lista_produtos)
+    planilha['SEO'] = seo_produtos
+    caminho_nova_planilha = os.path.join(caminho_pasta, 'new_sheet.xlsx')
+    planilha.to_excel(caminho_nova_planilha, index=False)
 
-planilha['SEO'] = seo_produtos
-
-planilha.to_excel('new_sheet.xlsx', index=False)
+except Exception as e:
+    print(f"Ocorreu um erro: {e}")
